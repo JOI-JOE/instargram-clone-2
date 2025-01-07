569729baff60792dbcfa954dad28053fb20c3bd0
JOI-JOE <iknowhtml161gmail.com>
2025-01-07 09:13:34 +0700

    Inite project

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
+</script>
+
+<template>
+    <MainLayout>
+        <div class="bg-green-500">Home.VUE</div>
+    </MainLayout>
+</template>
